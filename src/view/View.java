343f6e443ae7a3983565558b9df77b22f7514f17
23130175_TranBuuLoc <23130175@st.hcmuaf.edu.vn>
2026-06-18 11:14:39 +0700

    chỉnh sửa Home lần 1

--- src/view/View.java
+++ src/view/View.java
@@ -3,6 +3,9 @@ package view;
 public abstract class View {
     public  abstract void show() throws Exception;
     public  void showError(String error){
-        System.out.println(error);
+        System.out.println("------------------------");
+        System.out.println("SYSTEM ERROR :"+error);
+        System.out.println("------------------------");
     }
+
 }
