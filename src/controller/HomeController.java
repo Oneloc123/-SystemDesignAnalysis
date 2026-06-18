601d74cbd6faf856554beb8fb2a3a707ad0d907b
NgocAnh998 <23130017@st.hcmuaf.edu.vn>
2026-06-19 00:27:21 +0700

    fix

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -5,21 +5,23 @@ import view.HomeView;
 
 public class HomeController {
     HomeView hv;
-    // login thanhf confg
+    CalcSalaryController calcSalaryController;
 
     public HomeController() {
         this.hv = new HomeView(this);
+        this.calcSalaryController = new CalcSalaryController();
     }
+
     public void show() throws Exception {
         hv.show();
     }
 
-    public void excuteComent(String question) {
+    public void excuteComent(String question) throws Exception {
         switch(question) {
             case "1":
                 hv.printAddress();
                 System.out.println("Chuc nang 1 dang thuc hien");
-                // thuc thi 1
+                function();
                 break;
             case "2":
                 System.out.println("Chuc nang 2 dang thuc hien");
@@ -29,12 +31,13 @@ public class HomeController {
                 break;
         }
     }
-    public void function(){
-        CalcSalaryController cc = new CalcSalaryController();
 
-        if(!cc.execute(MainController.currentUser)){
+    public void function() throws Exception {
+        if (!calcSalaryController.checkRole(MainController.currentUser)) {
             hv.showError("Khong co quyen");
+            return;
         }
+        calcSalaryController.execute(MainController.currentUser);
     }
 
 }
