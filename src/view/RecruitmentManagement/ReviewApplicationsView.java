61e862bc7c63f574cfcda7842e412cc2bc4ee9f3
NgocAnh998 <23130017@st.hcmuaf.edu.vn>
2026-06-23 15:48:19 +0700

    add mniss file

--- src/view/RecruitmentManagement/ReviewApplicationsView.java
+++ src/view/RecruitmentManagement/ReviewApplicationsView.java
@@ -29,7 +29,7 @@ public class ReviewApplicationsView extends View {
             }
             controller.handleMainMenuCommand(question);
         }
-        return false;
+//        return false;
     }
 
     public void displayJobPostingList(List<JobPosting> jobPostings) throws Exception {
